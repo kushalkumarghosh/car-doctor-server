6f9372c470228d811832a4b9c98e00d531663db0
kushalkumarghosh <cse.kushal1@gmail.com>
2024-04-08 16:42:08 +0600

    updated

--- index.js
+++ index.js
@@ -28,6 +28,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection  = client.db('carmedic').collection('services');
+
+    app.get('/services', async(req, res)=>{
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/services/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = { _id: new Object(id)}
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -35,7 +51,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
